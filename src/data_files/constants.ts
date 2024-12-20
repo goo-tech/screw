223d8ad206b4ca72b6a643c096f5a5b469396fc3
goo-tech <knoacc@gmail.com>
2024-12-21 04:57:59 +0700

    Update constants.ts

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,12 +1,12 @@
 import ogImageSrc from "@images/social.png";
 
 export const SITE = {
-  title: "ScrewFast",
+  title: "GooTech",
   tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  description: "GooTech offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
+  description_short: "GooTech offers top-tier hardware tools and expert construction services to meet all your project needs.",
+  url: "https://www.gootech.my.id",
+  author: "Jon Mukidi",
 };
 
 export const SEO = {
@@ -34,6 +34,6 @@ export const OG = {
   type: "website",
   url: SITE.url,
   title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  description: "Equip your projects with GooTech top-quality hardware tools and expert construction services. Trusted by industry leaders, GooTech offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
   image: ogImageSrc,
 };
